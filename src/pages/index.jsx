0ca34f2975a91d078dbc0d453dd985f4e115fa29
Shahriyor <shahriyor@example.com>
2025-09-05 20:58:42 +0500

    ,

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,26 +1,52 @@
+// import React from "react";
+// import { Route, Routes } from "react-router-dom";
+// import Home from "./home";
+// import User from "./user";
+// import Login from "./login";
+// import MainLaylout from "./layout";
+// import ProductDetail from "./productDetail";
+
+// const AppRouter = () => {
+//   return (
+//     <>
+//       <Routes>
+//         <Route path="/" element={<MainLaylout />}>
+//           <Route path="/" element={<Home />} />
+//           <Route path="/home/:id" element={<Home />} />
+//           <Route path="/user" element={<User />} />
+//           <Route path="/user/:id" element={<User />} />
+//           <Route path="*" element={<div>404</div>} />
+//           <Route path="product/:id" element={<ProductDetail />} />
+//         </Route>
+//         <Route path="/login" element={<Login />} />
+//       </Routes>
+//     </>
+//   );
+// };
+
+// export default AppRouter;
+
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./home";
 import User from "./user";
 import Login from "./login";
-import MainLaylout from "./layout";
+import MainLayout from "./layout";
 import ProductDetail from "./productDetail";
 
 const AppRouter = () => {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<MainLaylout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/home/:id" element={<Home />} />
-          <Route path="/user" element={<User />} />
-          <Route path="/user/:id" element={<User />} />
-          <Route path="*" element={<div>404</div>} />
-          <Route path="product/:id" element={<ProductDetail />} />
-        </Route>
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<Home />} />
+        <Route path="products/:id" element={<Home />} />
+        <Route path="user" element={<User />} />
+        <Route path="user/:id" element={<User />} />
+        <Route path="product/:id" element={<ProductDetail />} />
+        <Route path="*" element={<div>404</div>} />
+      </Route>
+      <Route path="/login" element={<Login />} />
+    </Routes>
   );
 };
 
