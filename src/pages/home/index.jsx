53cac42888d0ccfb2f7135e8812d4a7500ceaf6d
Shahriyor <shahriyor@example.com>
2025-09-05 20:34:03 +0500

    .

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -10,10 +10,10 @@ const Home = () => {
   useEffect(() => {
     if (id) {
       axios
-        .get(`https://dummyjson.com/users/${id}`)
+        .get(`https://dummyjson.com/products/${id}`)
         .then((res) => setData({ users: [res.data] }));
     } else {
-      axios.get("https://dummyjson.com/users").then((res) => setData(res.data));
+      axios.get("https://dummyjson.com/products").then((res) => setData(res.data));
     }
   }, [id]);
 
@@ -21,22 +21,22 @@ const Home = () => {
     <div className="p-6">
       <h2 className="text-2xl font-bold mb-6">Our Humans</h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-        {data?.users?.map((food) => (
+        {data?.products?.map((food) => (
           <div key={food.id}>
             <div
               onClick={() => navigate(`/home/${food.id}`)}
               className="border rounded-2xl shadow-green-500 hover:shadow-2xl cursor-pointer overflow-hidden bg-white transition"
             >
               <img
-                src={food.image}
+                src={food.thumbnail}
                 width={300}
                 alt=""
                 className="w-full object-cover"
               />
             </div>
             <div className="text-green-500 font-bold  text-center">
-              <h3>{food.firstName}</h3>
-              <p className="text-green-700">{food.lastName}</p>
+              <h3>{food.title}</h3>
+              <p className="text-green-700">{food.price}</p>
             </div>
           </div>
         ))}
